482e15fce2d6fdd21bf414874f81e50296ce69dd
AwaisAhmedShahid <awaisahmedshahid@gmail.com>
2025-11-20 22:14:57 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <div className="w-full bg-amber-300 text-center text-2xl">test</div>
+    </div>
+  );
+}
+
+export default App;
